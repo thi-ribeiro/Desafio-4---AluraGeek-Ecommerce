19b360bec6df870381714ab519bd116c7d8671b5
Thiago Ribeiro Fantin <thiribeir@yahoo.com>
2022-10-02 20:43:57 -0300

    renamed:    Adicionar_produto.html -> adicionar.html 	modified:   assets/db/db.json 	modified:   assets/js/site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,30 +1,59 @@
 const botaoLogin = document.getElementById('login_btn');
 const loginForm = document.getElementById('login_form');
 
-botaoLogin.addEventListener('click', (e) => {
-	// console.log(e.target);
-	location.href = 'login.html';
-});
+const postProduto = document.getElementById('postProduto');
 
-loginForm.addEventListener('submit', (e) => {
-	e.preventDefault();
+console.log(postProduto);
 
-	let usuario = e.target;
+if (botaoLogin) {
+	botaoLogin.addEventListener('click', (e) => {
+		// console.log(e.target);
+		location.href = 'login.html';
+	});
+}
 
-	console.log(usuario);
+if (loginForm) {
+	loginForm.addEventListener('submit', (e) => {
+		e.preventDefault();
 
-	fetch('http://localhost:3000/profile')
-		.then((response) => {
-			if (response.status === 200) {
-				return response.json();
-			} else {
-				throw new Error('Something went wrong on api server!');
-			}
+		let usuario = e.target;
+
+		console.log(usuario);
+
+		fetch('http://localhost:5000/profile')
+			.then((response) => {
+				if (response.status === 200) {
+					return response.json();
+				} else {
+					throw new Error('Something went wrong on api server!');
+				}
+			})
+			.then((data) => {
+				console.log(data);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	});
+}
+
+if (postProduto) {
+	postProduto.addEventListener('submit', (e) => {
+		e.preventDefault();
+		//console.log(e.target[0]);
+
+		let nome = e.target[0].value;
+		let preco = e.target[1].value;
+		let tipo = e.target[2].value;
+
+		fetch('http://192.168.18.7:5000/produtos', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ produto: nome, tipo: tipo, preco: preco }),
 		})
-		.then((data) => {
-			console.log(data);
-		})
-		.catch((error) => {
-			console.error(error);
-		});
-});
+			.then((res) => res.json())
+			.then((data) => console.log('a'));
+	});
+}
